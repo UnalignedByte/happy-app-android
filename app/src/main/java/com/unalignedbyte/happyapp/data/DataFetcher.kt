d197695eb039f6770a7b41f9187a7ac2b7f87b92
Rafal Grodzinski <rafal@unalignedbyte.com>
2018-07-16 19:02:27 +0100

    Added interface for DataFetcher

--- app/src/main/java/com/unalignedbyte/happyapp/data/DataFetcher.kt
+++ app/src/main/java/com/unalignedbyte/happyapp/data/DataFetcher.kt
@@ -2,8 +2,12 @@ package com.unalignedbyte.happyapp.data
 
 import io.reactivex.Observable
 
-class DataFetcher {
-    fun fetchHappinessStatusJsonData(): Observable<Boolean> {
+interface DataFetcherProtocol {
+    fun fetchHappinessStatusJsonData(): Observable<Boolean>
+}
+
+class DataFetcher: DataFetcherProtocol {
+    override fun fetchHappinessStatusJsonData(): Observable<Boolean> {
         return Observable.just(false)
     }
 }
--- app/src/test/java/com/unalignedbyte/happyapp/DataFetcherTests.kt
+++ app/src/test/java/com/unalignedbyte/happyapp/DataFetcherTests.kt
@@ -4,6 +4,7 @@ import org.junit.Test
 import io.reactivex.observers.TestObserver
 import com.unalignedbyte.happyapp.data.DataFetcher
 
+
 class DataFetcherTests {
     @Test fun testFetchHappinessStatusWithoutWebApi() {
         val dataFetcher = DataFetcher()
